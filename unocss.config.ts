149c753adae8cfe6ba28dd3849a6df894e3a8f3b
JimYaaa <jimyaaa@JimYaaadeMacBook-Pro.local>
2023-08-04 14:30:53 +0800

    Spliter Project Init

---
+++ unocss.config.ts
@@ -0,0 +1,19 @@
+import {
+    defineConfig,
+    presetUno,
+    presetAttributify,
+    presetIcons,
+} from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
+
+export default defineConfig({
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons({
+            scale: 1.2,
+        }),
+    ],
+
+    transformers: [ transformerDirectives() ]
+})
